e41640433309a5d01532ece4bf8a1414e4bba44d
Gustavo Melo <gustavo.dsantosmelo@gmail.com>
2021-11-02 10:40:22 -0300

    Basic components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,16 @@
+import NavBar from "components/NavBar";
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+
 function App() {
   return (
-    <div>
-      <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
-        <div className="container">
-          <nav className="my-2 my-md-0 mr-md-3">
-            <img src={ImgDsDark} alt="DevSuperior" width="120" />
-          </nav>
-        </div>
+    <>
+      <NavBar></NavBar>
+      <div className="container">
+        <DataTable></DataTable>
       </div>
-      <footer className="footer mt-auto py-3 bg-dark">
-        <div className="container">
-          <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Nelio Alves</a></p>
-          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
-            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
-        </div>
-    </footer>
-    </div>
+      <Footer></Footer>
+    </>
   );
 }
 
