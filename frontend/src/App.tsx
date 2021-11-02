c31522a8d7c2a12c73974a439ead5695bfc6378f
Gustavo Melo <gustavo.dsantosmelo@gmail.com>
2021-11-02 10:03:12 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
 function App() {
   return (
     <div>
-      <h1>Olá mundo!</h1>
+      <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+        <div className="container">
+          <nav className="my-2 my-md-0 mr-md-3">
+            <img src={ImgDsDark} alt="DevSuperior" width="120" />
+          </nav>
+        </div>
+      </div>
+      <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Nelio Alves</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+        </div>
+    </footer>
     </div>
   );
 }
