f2218bb6249849f98fde361019e1deaaf1282736
Gustavo Melo <gustavo.dsantosmelo@gmail.com>
2021-11-06 10:14:46 -0300

    Routes

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,26 +1,44 @@
-import Chart from 'react-apexcharts';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Chart from 'react-apexcharts'
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
 
-function DonutChart() {
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
-    
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
+
+const DonutChart = () => {
+
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sallerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, [])
+
     const options = {
         legend: {
             show: true
         }
     }
-
     return (
-      <Chart 
-        options={{...options, labels: mockData.labels}}
-        series={mockData.series}
-        type="donut"
-        height="240"
-      />
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
+
+        />
     );
-  }
-  
-  export default DonutChart;
-  
+}
+
+export default DonutChart;
--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,42 +1,12 @@
-function Footer() {
-  return (
-    <footer className="footer mt-auto py-3 bg-dark">
-      <div className="container">
-        <p className="text-light">
-          App desenvolvido por{" "}
-          <a
-            href="https://github.com/Gustavo-Melo-dev"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Gustavo Melo
-          </a>
-        </p>
-        <p className="text-light">
-          <small>
-            <br />
-            Instagram:{" "}
-            <a
-              href="https://instagram.com/guumelo"
-              target="_blank"
-              rel="noreferrer"
-            >
-              @guumelo
-            </a>
-            <br />
-            LinkedIn:{" "}
-            <a
-              href="https://www.linkedin.com/in/gustavo-melo-a3ba73199/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Gustavo Melo
-            </a>
-          </small>
-        </p>
-      </div>
-    </footer>
-  );
+const Footer = () => {
+    return (
+       <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.instagram.com/guumelo/" target="_blank" rel="noreferrer">Gustavo Melo</a></p>
+                <p className="text-light">Linkedin <a href="https://www.linkedin.com/in/gustavo-melo-a3ba73199/" target="_blank" rel="noreferrer">Gustavo Melo</a></p>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
